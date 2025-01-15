7b079642669fee97b90ab078fdbff3b94c58a6ba
acscoder-digitalonda <acscoder@digitalonda.com>
2025-01-15 11:37:09 +0700

    new

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 use struct_to_json_db::*;
-auto_json_db_config!("./db/");
+use serde::{Deserialize, Serialize};
 
-#[auto_json_db]
+use dotenv::dotenv; 
+
+#[auto_json_db(singleton,encript="APP_SECRET_KEY")]
+struct SiteConfig {
+    url: String,
+    limit: i32
+}
+impl SiteConfig {
+    fn default()->Self{
+        Self::new("".to_owned(),0) 
+    }
+}
+
+#[auto_json_db(bigsize,unique="title")]
 struct Post {
     title: String,
-    description: String
+    description: String,
+    categories: Vec<u64>
 }
-#[auto_json_db]
+#[auto_json_db(unique="name")]
 struct Category {
     name: String
 }
-struct_db_relation!(Post, Category);
-
+json_db_relation!(Post=categories, Category);
+ 
 fn main() {
-    let all_posts = Post::get_all(); 
-    println!("{:?}", all_posts);
+    dotenv().ok();
+    set_struct_json_path("./db/");
 
-    let name_val = "test".to_string();
-    let p1 = db_relation_get!(Post=1, Category);
+    let mut cf = SiteConfig::load();
+     
+    println!("{:?}",  cf);
+    
+    let mut all_posts = Post::get_all(); 
+    println!("{:?}", all_posts.len());
+    
+    let c1 = Category::new("cat_1".to_owned());
+    let c2 =  Category::new("cat_2".to_owned());
+    c1.save();
+    c2.save();
+   
+    let c = add_posts("post_2".to_owned(), "desc_1".to_owned(), &vec![c1,c2]);
+    println!("{:?}", c);
+    
 }
-fn add_posts(title: String, description: String) {
-    let p1 = Post::new( title,description);
-    p1.save();
+fn add_posts(title: String, description: String,cates:&Vec<Category>)->Option<u64> {
+    let mut p1 = Post::new( title,description,vec![]);
+    if cates.len() > 0{
+        p1.set_categories(cates);
+    }
+    p1.save()
 }
